1aae2782a0e3abfa9a91cc09a549b89263dccf73
Marcio Marques de Souza <marcio.souza@dextra-sw.com>
2019-09-29 23:42:42 -0300

    first commit

---
+++ src/screens/EditScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const EditScreen = () => {
+    return (
+        <View>
+            <Text>Edit Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default EditScreen;
